36ecadd39425e7a6b9019b4750a4e40cf40448f6
David Castellar Martínez <castellardav@gmail.com>
2022-03-12 16:58:44 -0500

    Start with socket.io

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 const port = 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+
+io.on('connection', () => {
+   console.log('New WebSocket connection');
+});
 
 server.listen(port, () => {
    console.log(`App listening on port ${port}`);
