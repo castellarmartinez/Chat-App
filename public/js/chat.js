5a5dde7b4db49b0ff720ac1771171e800772abcd
David Castellar Martínez <castellardav@gmail.com>
2022-03-14 16:26:13 -0500

    Send first message with socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,6 @@
-io()
+const socket = io();
+
+socket.on('countUpdate', (count) => {
+   console.log('The count has been update');
+   console.log(count);
+});
--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
    console.log('New WebSocket connection');
+
+   let count = 0;
+   socket.emit('countUpdate', count);
 });
 
 server.listen(port, () => {
