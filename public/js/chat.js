e78c4dd7ab84d70fafd9731b50eabc4f69d883ca
David Castellar Martínez <castellardav@gmail.com>
2022-03-14 17:00:06 -0500

    Refactorizes the way the message send by the user is accessed

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,6 +7,6 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
    e.preventDefault();
 
-   const message = document.querySelector('#message').value;
+   const message = e.target.elements.message.value;
    socket.emit('userMessage', message);
 });
