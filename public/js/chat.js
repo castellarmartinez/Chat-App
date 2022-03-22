c9283039ab3376720b6c80883624f948fbcf5015
David Castellar Martínez <castellardav@gmail.com>
2022-03-22 09:44:09 -0500

    Disables send button while sending a message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,23 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('button');
+
 socket.on('message', (message) => {
    console.log(message);
 });
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
    e.preventDefault();
+   $messageFormButton.setAttribute('disabled', 'disabled');
 
    const message = e.target.elements.message.value;
-   
+
    socket.emit('userMessage', message, (error) => {
+      $messageFormButton.removeAttribute('disabled');
+
       if (error) {
          return console.log(error);
       }
